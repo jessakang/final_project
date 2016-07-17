951395ce7b7997509cdb49bdfdd2027c0f28b266
jessakang <kang.jessicaa@gmail.com>
2016-07-17 17:51:22 +1000

    adding

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # final_project
+
+Created a data visualization map which shows the gender pay gap from a range of countries from 2000-2014. This data was collected from the Organisation for Economic Co-operation and Development. Using a SVG map and D3, I was able to render this data into line graphs in the window.
--- main.js
+++ main.js
@@ -1,164 +1,251 @@
-var showCountryData = function (country) {
-  // debugger;
+var countryArray = [];
 
-  var margin = {
-          top: 70,
-          right: 20,
-          bottom: 30,
-          left: 50
-      },
-      w = 1000 - margin.left - margin.right,
-      h = 500 - margin.top - margin.bottom;
+// storing the entire d3 chart formation in a function to render onto the page
 
-  var parseDate = d3.time.format("%y").parse;
+var showCountryData = function(country, countryNumber) {
+    // debugger;
 
-  var x = d3.scale.linear()
-      .domain([2000, 2014])
-      .range([0, w]);
+    // Appending the function to the popup div which then gets appended to the body
+    var popup = $("<div id='popup'><span id='close'>x</span></div>");
+    if ($("#popup").length === 0) {
+        $('body').append(popup);
+    }
+    var margin = {
+            top: 70,
+            right: 20,
+            bottom: 30,
+            left: 50
+        },
+        w = 1000 - margin.left - margin.right,
+        h = 500 - margin.top - margin.bottom;
 
-  var y = d3.scale.linear()
-      .domain([0, 50])
-      .range([h, 0]);
+    var parseDate = d3.time.format("%y").parse;
 
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom")
-      .tickFormat(d3.format("data"))
-      .ticks(15);
+    // Setting up domains on the x and y axis. Linear because we are dealing with numbers only.
+    var x = d3.scale.linear()
+        .domain([2000, 2014])
+        .range([0, w]);
 
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .tickFormat(function(d) {
-          return d + "%";
-      })
-      .ticks(5);
+    var y = d3.scale.linear()
+        .domain([0, 50])
+        .range([h, 0]);
 
-  var tip = d3.tip()
-      .attr("class", "d3-tip")
-      .offset([-10, 0])
-      .html(function(data) {
-          return "<span style='color:steelblue'>" + data.Value + "%" + "</span>";
-      })
+    // Setting up the orientation of the x and y axis.
 
-  var xGrid = d3.svg.axis()
-      .scale(x)
-      .orient("bottom")
-      .ticks(15)
-      .tickSize(-h, 0, 0)
-      .tickFormat("");
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .tickFormat(d3.format("data"))
+        .ticks(15);
 
-  var yGrid = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .ticks(5)
-      .tickSize(-w, 0, 0)
-      .tickFormat("");
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(function(d) {
+            return d + "%";
+        })
+        .ticks(5);
 
-  var svg = d3.select("#popup").append("svg")
-      .attr("width", w + margin.left + margin.right)
-      .attr("height", h + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+    // Using the library d3-tip to add tooltips to the d3 line graph. This is where I will be displaying the y-values for each plot point.
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + h + ")")
-      .call(xAxis)
-      .append("text")
-      .attr("x", 458)
-      .attr("y", 30)
-      .attr("dx", ".71em")
-      .style("text-anchor", "middle")
-      .text("Year");
+    var tip = d3.tip()
+        .attr("class", "country" + countryNumber)
+        .offset([-10, 0])
+        .html(function(data) {
+            return "<span style='color:steelblue'>" + data.Value + "%" + "</span>";
+        })
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("x", 0 - (h / 2))
-      .attr("y", 0 - margin.left)
-      .attr("dy", ".71em")
-      .style("text-anchor", "middle")
-      .text("Percentage");
+    // Creating a grid system that will distribute plot points evenly on the chart.
 
-  svg.append("g")
-      .attr("class", "grid")
-      .attr("transform", "translate(0," + h + ")")
-      .call(xGrid);
+    var xGrid = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(15)
+        .tickSize(-h, 0, 0)
+        .tickFormat("");
 
-  svg.append("g")
-      .attr("class", "grid")
-      .call(yGrid);
+    var yGrid = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(5)
+        .tickSize(-w, 0, 0)
+        .tickFormat("");
 
-  d3.csv("GENDER_EMP_11072016050959084.csv", function(error, data) {
-      // debugger;
-      data = _.groupBy(data, "Country");
-      data = data[country];
-      // debugger
-      data.forEach(function(data) {
-          // debugger
-          data.time = parseDate(data.TIME);
-          data.value = +data.Value;
-      });
+    // Appending the svg elements to the popup div, which will display the line chart in it.
 
-      // debugger;
-      x.domain(d3.extent(data, function(data) {
-          return data.TIME;
-      }));
+    var svg = d3.select("#popup").append("svg")
+        .attr("width", w + margin.left + margin.right)
+        .attr("height", h + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-      var line = d3.svg.line()
-          .x(function(data) {
-              return x(data.TIME);
-          })
-          .y(function(data) {
-              return y(data.Value);
-          });
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + h + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("x", 458)
+        .attr("y", 30)
+        .attr("dx", ".71em")
+        .style("text-anchor", "middle")
+        .text("Year");
 
-      var title = svg.append("g")
-          .attr("class", "title");
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", 0 - (h / 2))
+        .attr("y", 0 - margin.left)
+        .attr("dy", ".71em")
+        .style("text-anchor", "middle")
+        .text("Percentage");
 
-      title.append("text")
-          .attr("x", (w / 2))
-          .attr("y", -30)
-          .attr("text-anchor", "middle")
-          .style("font-size", "16px")
-          .text("*The gender pay gap is defined as the difference between male and female median wages divided by the male median wages.");
+    svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + h + ")")
+        .call(xGrid);
 
-      svg.append("path")
-          .datum(data)
-          .attr("class", "line")
-          .attr("d", line);
+    svg.append("g")
+        .attr("class", "grid")
+        .call(yGrid);
 
-      svg.selectAll(".circle")
-          .data(data)
-          .enter()
-          .append("svg:circle")
-          .attr("class", "circle")
-          .attr("cx", function(data) {
-              return x(data.TIME);
-          })
-          .attr("cy", function(data) {
-              return y(data.Value);
-          })
-          .attr("r", 5)
-          .on('mouseover', tip.show)
-          .on('mouseout', tip.hide)
-          .call(tip)
+    // We are collecting data from a CSV file.
 
-  });
+    d3.csv("GENDER_EMP_11072016050959084.csv", function(error, data) {
+        // debugger;
+        data = _.groupBy(data, "Country");
+        // Collecting the data by Country name.
+        console.log(country);
+        data = data[country];
+        // debugger
+        data.forEach(function(data) {
+            // For each country, we will need to grab the years as well as the values (percentages) for corresponding years.
+            // debugger
+            data.time = parseDate(data.TIME);
+            data.value = +data.Value;
+        });
 
-  $('path').on("click", function(){
-      country = this.getAttribute("aria-label");
-      // debugger
-      console.log( country );
+        // debugger;
+
+        // Appending the years along the x axis
+        x.domain(d3.extent(data, function(data) {
+            return data.TIME;
+        }));
+
+        // Creating the line
+        var line = d3.svg.line()
+            .x(function(data) {
+                return x(data.TIME);
+            })
+            .y(function(data) {
+                return y(data.Value);
+            });
+        // Adding a title to the line chart
+        var title = svg.append("g")
+            .attr("class", "title");
+
+        title.append("text")
+            .attr("x", (w / 2))
+            .attr("y", -30)
+            .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .text("*The gender pay gap is defined as the difference between male and female median wages divided by the male median wages.");
+
+        // Appending a line path joining all points on the line
+        svg.append("path")
+            .datum(data)
+            .attr("class", "line countryLine" + countryNumber)
+            .attr("d", line);
+
+        // Append country's name to the line path
+        // svg.append("text")
+        //     .attr("transform", "translate(" + (w+3) + "," + y(data[0].Country) + ")")
+        //     .attr("dy", ".35em")
+        //     .attr("text-anchor", "start")
+        //     .style("fill", "green")
+        //     .text("Canada");
+
+        // Selecting all circle points and displaying their y values using mouseover
+        var plotpoints = svg.selectAll("circle.country" + countryNumber)
+            .data(data)
+            .enter()
+            .append("svg:circle")
+            .attr("class", "country" + countryNumber)
+            .attr("cx", function(data) {
+                return x(data.TIME);
+            })
+            .attr("cy", function(data) {
+                return y(data.Value);
+            })
+            .attr("r", 5)
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide);
+        // console.log( test.length );
+
+        plotpoints.call(tip)
     });
-  };
+};
+// When a country is clicked on the map, take the title of that country and return it to the showCountryData function
 
-  $('body').click(function() {
-  if (!$(this.target).is('#popup')){
-     $("#popup").fadeOut(300);
-     $("#popup").addClass('hide');
-  }
+map.addListener("clickMapObject", function(event) {
+    event.event.stopPropagation();
+    event.event.stopImmediatePropagation();
+    country = event.mapObject.title
+
+    // When the toggle button is switched (true), each country clicked gets pushed into the countryArray
+    if (multipick) {
+        // console.log("multipick ran")
+
+        // add another if statement to make sure the country you are pushing is unique. if indexOf ...
+        // var unique = [];
+        //   $.each(country, function(i, el){
+        //     if($.inArray(el, unique) === -1) unique.push(el);
+        //     });
+
+        var arrayLength = countryArray.length;
+        countryArray.push(country);
+
+        if (countryArray.length < 2) {
+            return false;
+        } else {
+            console.log(countryArray);
+            showCountryData(countryArray[0], 0);
+            // window.setTimeout(function () {
+            showCountryData(countryArray[1], 1);
+            // }, 1000);
+            return true;
+        }
+        // debugger
+    }
+    showCountryData(country, 0);
+
 });
+$('body').on("click", "#close", function() {
+    // if the array is equal or greater to two, clear the array. Refresh!
+    if (countryArray.length >= 2) {
+        countryArray = [];
+    }
+    $("#popup").remove();
+});
+// By default, the multipick is false until it is clicked
+
+var multipick = false;
+
+$(document).ready(function() {
+    // Switch click handler
+
+    var clickedButton = _.debounce(function() {
+        if (multipick) {
+            multipick = false;
+            // console.log("Tell me lies, tell me sweet little lies...");
+        } else {
+            multipick = true;
+            // console.log("True that... DOUBLE TRUE.");
+        }
+    }, 300)
+
+    // listener
+    $(".switch").on("click", clickedButton)
+
+})
